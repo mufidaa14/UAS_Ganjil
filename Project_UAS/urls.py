756e2cbdf1644b2e02ccee413cc870e6afda69b4
mufidaa14 <mufidajeje14@gmail.com>
2023-12-26 23:28:48 +0700

    Penambahan jadwal hari senin

--- Project_UAS/urls.py
+++ Project_UAS/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path("main/", views.main, name="main"),
+    path("jadwal/", views.jadwal, name="jadwal"),
+    path("senin/", views.senin, name="senin"),
+    path("selasa/", views.selasa, name="selasa"),
+    path("kamis/", views.kamis, name="kamis"),
+    path("jumat/", views.jumat, name="jumat"),
+    path("profil/", views.profil, name="profil"),
+    path("contact/", views.contact, name="contact"),
 ]
--- Project_UAS/views.py
+++ Project_UAS/views.py
@@ -7,3 +7,24 @@ from django.template import loader
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+def jadwal(request):
+    template = loader.get_template('jadwal.html')
+    return HttpResponse(template.render())
+def senin(request):
+    template = loader.get_template('senin.html')
+    return HttpResponse(template.render())
+def selasa(request):
+    template = loader.get_template('selasa.html')
+    return HttpResponse(template.render())
+def kamis(request):
+    template = loader.get_template('kamis.html')
+    return HttpResponse(template.render())
+def jumat(request):
+    template = loader.get_template('jumat.html')
+    return HttpResponse(template.render())
+def profil(request):
+    template = loader.get_template('profil.html')
+    return HttpResponse(template.render())
+def contact(request):
+    template = loader.get_template('contact.html')
+    return HttpResponse(template.render())
